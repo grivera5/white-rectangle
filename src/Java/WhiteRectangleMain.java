db9c02250e6736b53a376fc40f9d40e4d87bfd4a
Chris <cjacocks@daltonstate.edu>
2023-04-30 15:12:46 -0400

    Adding some of the main menu pages. No practical Functionality yet.

--- src/Java/WhiteRectangleMain.java
+++ src/Java/WhiteRectangleMain.java
@@ -1,12 +1,25 @@
 package Java;
 
+import javax.print.attribute.standard.Media;
+import java.io.File;
+
 public class WhiteRectangleMain {
     /*
     This class file should serve as the primary contoller for all other classes. It will launch the game menu
     and once a game is selected, will initiate any functions or methods needed to serve the player with that game.
+
+    This class may not even be needed. TBD
      */
+
+    // TODO - Add Initial start page that asks user whether they want to play and new game, load a game, or view scoreboards
+    // TODO - Add New Game select page
+    // TODO - Add Load Game select Page
+    // TODO - Add View High Scores Game Select Page
+
 
     public static void main(String[]args) {
         System.out.println("WhiteRectangle: Running Main Method Now");
+
+        // TODO -- Make this run the start page
     }
 }
