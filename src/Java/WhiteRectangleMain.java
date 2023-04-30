52c0dcb9799b4a6ce27d9f80b77fb26a73d70f30
Chris <cjacocks@daltonstate.edu>
2023-04-30 14:18:25 -0400

    Added sub directories for java files and resources. Refactored as needed and added the main class.

---
+++ src/Java/WhiteRectangleMain.java
@@ -0,0 +1,12 @@
+package Java;
+
+public class WhiteRectangleMain {
+    /*
+    This class file should serve as the primary contoller for all other classes. It will launch the game menu
+    and once a game is selected, will initiate any functions or methods needed to serve the player with that game.
+     */
+
+    public static void main(String[]args) {
+        System.out.println("WhiteRectangle: Running Main Method Now");
+    }
+}
